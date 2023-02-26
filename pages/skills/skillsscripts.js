f2a06b5600fb8bbab5ce0149742eb1076c4a3bb4
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2023-02-26 15:55:58 +0530

    Update 1.3
    changelog:
    - added other pages with filler content

---
+++ pages/skills/skillsscripts.js
@@ -0,0 +1,9 @@
+const preloader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  this.setTimeout(preLoader, 1000);
+});
+
+function preLoader() {
+  preloader.classList.toggle("fade");
+  preloader.style.zIndex = "-1";
+}
