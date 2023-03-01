0a9689457c62dba3b5c988ec43163b07c19da21e
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2023-03-01 15:41:19 +0530

    test

--- pages/contact/contactscripts.js
+++ pages/contact/contactscripts.js
@@ -2,7 +2,6 @@ const preloader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   this.setTimeout(preLoader, 1000);
 });
-
 function preLoader() {
   preloader.classList.toggle("fade");
   preloader.style.zIndex = "-1";
