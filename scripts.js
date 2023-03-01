693451752ba4dde6fb836ad329880815e6bc8174
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2023-03-01 16:07:22 +0530

    blog eroro

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const preloader = document.getElementById("preloader");
 const handImg = document.getElementById("header");
+const blogOption = document.getElementById("blog");
 window.addEventListener("load", function () {
   this.setTimeout(preLoader, 1000);
   handImg.style.display = "none";
@@ -9,3 +10,7 @@ function preLoader() {
   preloader.classList.toggle("fade");
   preloader.style.zIndex = "-1";
 }
+
+blogOption.addEventListener("click", function () {
+  alert("Sorry, I'm still working on a blog page :)");
+});
