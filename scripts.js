c23e99292fe45e04189610dbdef85782369b8ea8
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2023-03-01 18:20:38 +0530

    sessionstorageupdate

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 const preloader = document.getElementById("preloader");
 const handImg = document.getElementById("header");
 const blogOption = document.getElementById("blog");
+const betaMessage = document.getElementById("message");
+const messageClose = document.getElementById("xmark");
+const mainBlur = document.getElementById("main");
+sessionStorage.setItem("firstload", "true");
+// let loadStats = sessionStorage.getItem("firstload");
 window.addEventListener("load", function () {
   this.setTimeout(preLoader, 1000);
   handImg.style.display = "none";
+  if (sessionStorage.getItem("firstload") === "true") {
+    sessionStorage.setItem("firstload", "false");
+    let loadStats = sessionStorage.getItem("firstload");
+    console.log(loadStats);
+    mainBlur.classList.toggle("blur");
+    messageClose.addEventListener("click", function () {
+      betaMessage.style.display = "none";
+      mainBlur.classList.toggle("blur");
+    });
+  } else {
+    betaMessage.style.display = "none";
+  }
 });
 
 function preLoader() {
